9b16fc20b7508e4df04655fdc7bd51c11b7907e7
Hendry535230151 <hendry.535230151@stu.untar.ac.id>
2024-11-26 20:13:10 +0700

    Success add login andd routing

---
+++ FRONTEND_PROGRAMMING_UAS/app/Http/Controllers/AuthController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email|string',
+            'password' => 'required|string',
+        ]);
+
+        $user = User::where('email', $request->email)->first();
+        \Log::info($user);
+
+        if ($user && Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Login berhasil',
+                'user' => [
+                    'id' => $user->id,
+                    'email' => $user->email,
+                ],
+            ]);
+        }
+
+        return response()->json([
+            'success' => false,
+            'message' => 'Email atau password salah',
+        ], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => 'Logout berhasil',
+        ]);
+    }
+}
--- FRONTEND_PROGRAMMING_UAS/bootstrap/app.php
+++ FRONTEND_PROGRAMMING_UAS/bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->web(append: [
+            \App\Http\Middleware\CorsMiddleware::class
+        ]);
+        $middleware->validateCsrfTokens(except:[
+            'login',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
