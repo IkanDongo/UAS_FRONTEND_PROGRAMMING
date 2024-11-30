aa22d599a97d9be1f3645eb34e3e94bbd65342cf
IkanDongo <noel.535230067@stu.untar.ac.id>
2024-11-30 20:58:51 +0700

    mengubah db:seeder karena terjadi error

--- FRONTEND_PROGRAMMING_UAS/database/seeders/DatabaseSeeder.php
+++ FRONTEND_PROGRAMMING_UAS/database/seeders/DatabaseSeeder.php
@@ -27,45 +27,15 @@ class DatabaseSeeder extends Seeder
             'created_at' => now()->toDateTimeString(),
             'updated_at' => now()->toDateTimeString(),
         ]);
-        // DB::table('users')->insert([
-        //     'name' => 'Admin',
-        //     'email' => 'Admin@example.com',
-        //     'password' => Hash::make('Admin123!'),
-        //     'is_admin' => true, // Set as admin
-        //     'email_verified_at' => now()->toDateTimeString(),
-        //     'remember_token' => Str::random(10),
-        //     'created_at' => now()->toDateTimeString(),
-        //     'updated_at' => now()->toDateTimeString(),
-        // ]);
-        DB::table('products')->insert([
-            [
-                'name' => 'Product 1',
-                'description' => 'Description for Product 1',
-                'category' => 'Category A',
-                'price' => 100.00,
-                'stock' => 50,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            // [
-            //     'name' => 'Product 2',
-            //     'description' => 'Description for Product 2',
-            //     'category' => 'Category B',
-            //     'price' => 150.00,
-            //     'stock' => 30,
-            //     'created_at' => now(),
-            //     'updated_at' => now(),
-            // ],
-        ]);
-        $user = \App\Models\User::first();
-        $product = \App\Models\Products::first(); 
-
-        DB::table('carts')->insert([
-            'user_id' => $user->id,
-            'product_id' => $product->id,
-            'quantity' => 2,
-            'created_at' => now(),
-            'updated_at' => now(),
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'Admin@example.com',
+            'password' => Hash::make('Admin123!'),
+            'is_admin' => true, // Set as admin
+            'email_verified_at' => now()->toDateTimeString(),
+            'remember_token' => Str::random(10),
+            'created_at' => now()->toDateTimeString(),
+            'updated_at' => now()->toDateTimeString(),
         ]);
     }
 }
