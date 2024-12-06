18d877cd0a4ebc95b2a2828d7769265248402ded
IkanDongo <noel.535230067@stu.untar.ac.id>
2024-12-06 12:58:13 +0700

    rename brand

--- FRONTEND_PROGRAMMING_UAS/resources/views/login.blade.php
+++ FRONTEND_PROGRAMMING_UAS/resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Document</title>
+    <title>WebStyle</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/1975de5d56.js" crossorigin="anonymous"></script>
